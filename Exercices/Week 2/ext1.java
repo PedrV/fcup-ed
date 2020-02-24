8e3481d5ccd4b0834d288cde68721acf0b776e92
PedrV <46421895+PedrV@users.noreplay.github.com>
2020-02-24 16:02:08 +0000

    organize directories

---
+++ Exercices/Week 2/ext1.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ext1 {
+
+    static void losangulo(final int n) {
+
+        int left_bound = n/2 + 1, right_bound = n/2 + 1;
+
+        // 1º parte do losangulo
+        for (int i = 1; i <= n/2 + 1; i++) {
+            for(int j = 1; j <= n; j++){
+                if(j >= left_bound && j <= right_bound){
+                    System.out.print("#");
+                }  else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+            left_bound--;
+            right_bound++;
+        }
+
+        // 2º parte do losangulo
+        left_bound += 2;
+        right_bound -= 2;
+
+        for (int i = n/2; i >= 1; i--) {
+            for(int j = 1; j <= n; j++){
+                if(j >= left_bound && j <= right_bound){
+                    System.out.print("#");
+                }  else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+            left_bound++;
+            right_bound--; 
+        }
+    }
+
+    public static void main(final String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        losangulo(scanner.nextInt());
+
+
+    }
+}
