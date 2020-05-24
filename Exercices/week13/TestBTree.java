8386de31fe2f11d0bd2d91545b4c5944703cb3a0
Pedro Vieira <46421895+PedrV@users.noreply.github.com>
2020-05-24 13:11:35 +0100

    Week13: ED233 Commit exercice, draft1

---
+++ Exercices/week13/TestBTree.java
@@ -0,0 +1,30 @@
+package week13;
+
+import java.util.Arrays;
+
+// -----------------------------------------------------------
+// Estruturas de Dados 2019/2020 (CC1007) - DCC/FCUP
+// http://www.dcc.fc.up.pt/~pribeiro/aulas/edados1920/
+// -----------------------------------------------------------
+// Exemplo de utilizacao da uma arvore binaria
+// Ultima alteracao: 26/04/2018
+// -----------------------------------------------------------
+
+import java.util.Scanner;
+
+public class TestBTree {
+   public static void main(String[] args) {
+      // Ler arvore de inteiros em preorder
+      Scanner in = new Scanner(System.in);
+      BTree<Integer> t = LibBTree.readIntTree(in);
+
+
+      // Escrever nos da arvore seguindo varias ordens possiveis
+      System.out.println(Arrays.toString(t.maxLevel()));
+
+/*      t.printInOrder();
+      t.printPostOrder();
+      t.printBFS();
+      t.printDFS(); */
+   }
+}
